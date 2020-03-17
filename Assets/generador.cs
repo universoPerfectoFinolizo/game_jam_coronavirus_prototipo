3d16e824a5dd75e7ba0a54cb7aeb951ba5a160fe
JarssS8 <47742360+JarssS8@users.noreply.github.com>
2020-03-17 22:58:42 +0100

    Update generador

--- Assets/generador.cs
+++ Assets/generador.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class generador : MonoBehaviour
@@ -16,32 +15,27 @@ public class generador : MonoBehaviour
     public float tiempo_generar;
 
     //tiempo que dura las oleadas:
-    public int tiempo_oleada1;
-    public int tiempo_oleada2;
+    public int[] tiempos_oleadas = new int[]{10,20,30} ;
 
     //tiempo en que dura el descanso:
     public float tiempo_descanso = 10f;
 
     //datos variados:
     int tiempo_oleada_n;
-    int nivel_orda_ant = 0;
 
     private void Start()
     {
-        StartCoroutine(generar(true));
+        generar();
     }
 
-    IEnumerator generar(bool p1)
+    private void generar()
     {
-        while (p1 == true)
+        while (nivel_orda <= 100)
         {
-            //detectar nivel orda:
-            if (nivel_orda == 1)
-            {
                 //tiempo oleada:
-                if (tiempo_oleada_n < tiempo_oleada1)
+                if (tiempo_oleada_n < tiempos_oleadas[nivel_orda-1])
                 {
-                    yield return new WaitForSeconds(tiempo_generar);
+                    StartCoroutine(esperarTiempo(tiempo_generar));
                     int r1 = Random.Range(0, 10);
                     print(r1);
 
@@ -58,52 +52,17 @@ public class generador : MonoBehaviour
                 }
                 else
                 {
-                    nivel_orda_ant = nivel_orda;
-                    nivel_orda = 100;
-                    tiempo_oleada_n = 0;
+                    StartCoroutine(esperarTiempo(tiempo_descanso));
                 }
-                
-            }
-            else if (nivel_orda==2)
-            {
-                if (tiempo_oleada_n < tiempo_oleada2)
-                {
-                    yield return new WaitForSeconds(tiempo_generar);
-                    int r1 = Random.Range(0, 10);
-                    print(r1);
-
-                    switch (r1)
-                    {
-                        case 0:
-                            GameObject persona1 = GameObject.Instantiate(persona, transform.position, transform.rotation) as GameObject;
-                            break;
-                        default:
-                            GameObject enemigo1_1 = GameObject.Instantiate(enemigo1, transform.position, transform.rotation) as GameObject;
-                            break;
-                    }
-                    tiempo_oleada_n++;
-                }
-                else
-                {
-                    nivel_orda_ant = nivel_orda;
-                    nivel_orda = 100;
-                    tiempo_oleada_n = 0;
-                }
-                
-            }
-            else if (nivel_orda == 100)
-            {
-                //modo descanso:
-                yield return new WaitForSeconds(tiempo_descanso);
-                nivel_orda = nivel_orda_ant;
-                nivel_orda++;
-            }
-            else
-            {
-                p1 = false;
-            }
-            
         }
+    }
+        
+    
+
+    private IEnumerator esperarTiempo (float tiempo){
+        yield return new WaitForSeconds(tiempo);
+        nivel_orda++;
+        tiempo_oleada_n = 0;
     }
 
 }
